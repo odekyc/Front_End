7be1d252f129451b1f1c6e08e28d2dc6a6ca6858
Kuan Chao <odekyc@gmail.com>
2016-04-19 00:29:08 -0700

    hi

--- Javascript_Calculator/javascript_calculator.js
+++ Javascript_Calculator/javascript_calculator.js
@@ -1,14 +1,33 @@
 var id;
+var text;
+var inner_div_id;
+var counter=0;
+
 $(document).ready(function() {
    $( "#AC" ).click(function() {
-  $( "#input" ).empty();
+  $("#input").empty();
 });
 
- $( ".keys" ).click(function() {
-     id=$(this).attr('id')
-     alert(id);
+ $(".keys").click(function() {
+     id=$(this).attr('id');
+
+     if(id==="AC"){
+        $('#input').empty();
+     }
+
+     
+     else if((Number(id)>=0)&&(Number(id)<=9)){
+        counter+=1;
+        if( counter===1){
+            document.getElementById('input').innerHTML=id;
+
+        }
+        else if(counter>=1){
+            text=document.getElementById('input').innerHTML;
+            text+=id;
+            document.getElementById('input').innerHTML=text;
+        }
+     }
 });
-
-
 
 });
