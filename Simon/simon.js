a55681d74f94708d355b5e24d66d1bd2fa094fa3
Elva Chao <odekyc@Elvas-Air.lan>
2016-07-08 09:19:31 -0700

    hi

--- Simon/simon.js
+++ Simon/simon.js
@@ -1,14 +1,39 @@
 $(document).ready(function() {
-   
+  
+var audio = $("audio")[0];
+var color_arr=["blue","green","red","yellow"];
+var result_arr=[];
+var user_clicked=[];
+var stricted=false;
+
 $("[name='my-checkbox']").bootstrapSwitch();
+
 
 $('#bootstrapswitch').on('switchChange.bootstrapSwitch', function (event, state) {
     
+  
     if(state===true){
     	$('#display').css('color', 'red');
+
+        $('#inner_strict').on('click',function(){
+           stricted=!stricted;
+           alert(stricted);
+            if(stricted===true){
+        	    $('#strict_alert').css('background-color', 'red');
+        	}
+            else if(stricted===false){
+        		$('#strict_alert').css('background-color', 'grey');
+       		 }
+           
+        });
+
+ 
+
     	$('#inner_start').on('click', function(){
           alert("inner_start clicked");
           $('#display').text('01');
+          audio.play();
+          
     	});
     }
 
