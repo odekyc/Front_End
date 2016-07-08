bfe0a5ec5086a0dc8f887f5fe50f6758a0200e47
Elva Chao <odekyc@Elvas-Air.lan>
2016-07-08 07:59:33 -0700

    updated

--- Simon/simon.js
+++ Simon/simon.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
-
+   
 $("[name='my-checkbox']").bootstrapSwitch();
 
+$('#bootstrapswitch').on('switchChange.bootstrapSwitch', function (event, state) {
+    
+    if(state===true){
+    	$('#display').css('color', 'red');
+    	$('#inner_start').on('click', function(){
+          alert("inner_start clicked");
+          $('#display').text('01');
+    	});
+    }
 
+    else if(state===false){
+    	$('#display').text('--');
+    	$('#display').css('color', '#330000');
+    }
+});
+
+// var cur_state=$('#bootstrapswitch').bootstrapSwitch('state');
+
+// alert(cur_state);
 
 });
