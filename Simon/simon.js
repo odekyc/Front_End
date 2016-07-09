a55bb10f8de5a2728182af964a1bcdd7c7377a7e
Elva Chao <odekyc@Elvas-Air.lan>
2016-07-08 17:32:42 -0700

    hi

--- Simon/simon.js
+++ Simon/simon.js
@@ -1,23 +1,45 @@
 $(document).ready(function() {
   
 var audio = $("audio")[0];
-var color_arr=["blue","green","red","yellow"];
+var orig_colors=["#004d99", "#006600" ,"#b30000", "#cccc00"];
+var color_arr=["blue","#009933","red","yellow"];
 var result_arr=[];
 var user_clicked=[];
 var stricted=false;
 var round_count=0;
-var random_num;
+var random_num=null;
 
 function gameStart(){
-   round_count+=1;
-   $('#display').text(round_count);
-   audio.play();
-   random_num=Math.floor((Math.random() * 3) + 1);
+
+	round_count+=1;
+    $('#display').text(round_count);
+    audio.play();
+   
+   	alert("random_num"+random_num);
+   	$('#'+color_arr[random_num]).css('background-color',orig_colors[random_num]);
+   
+   random_num=Math.floor(Math.random() * 3);
+   if(random_num===0){
+     $('#blue').css('background-color','blue');
+   }
+   else if(random_num===1){
+   	 $('#green').css('background-color','#009933');
+   }
+    else if(random_num===2){
+   	 $('#red').css('background-color','red');
+   }
+   else if(random_num===3){
+   	 $('#yellow').css('background-color','yellow');
+   }
+
+
+
    alert(random_num);
    result_arr.push(random_num);
 }
 
 function stopGame() {
+
     clearInterval(myVar);
 }
 
