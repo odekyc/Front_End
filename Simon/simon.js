f360dd60c3ebf5884f2ec4c12bca9d5d88ed07b8
Elva Chao <odekyc@Elvas-Air.championbroadband.com>
2016-07-24 19:19:49 -0700

    hi

--- Simon/simon.js
+++ Simon/simon.js
@@ -6,7 +6,9 @@ var orig_colors=["#004d99", "#004d1a" ,"#b30000", "#b3b300"];
 var color_arr=["blue","#00cc00","red","yellow"];
 var color_id=["blue","green","red","yellow"];
 var result_arr=[];
+var result_arrCounter=0;
 var user_clicked=[];
+var user_clickedCounter=0;
 var stricton=false;
 var running=false;
 var round_level=1;
@@ -14,6 +16,14 @@ var random_num=null;
 var poweron=false;
 var b4runblink;
 var gameInt;
+var respAlrtTimeOut;
+var gameIntTime=14700;
+var clicked_num=null;
+
+function win(){
+
+}
+
 
     $('#inner_strict').on('click',function(){
           
@@ -44,12 +54,20 @@ var gameInt;
               $('#display').text('--');
               clearInterval(b4runblink);
               clearTimeout(presetlv1);
+              round_level=1;
+              result_arr=[];
+              user_clicked=[];
+
              }
              else if(!running){
                 running=true;
                
                  $('#inner_strict').css("pointer-events", "none");
                 $('#inner_start').css("background-color", "green");
+
+                random_num=Math.floor((Math.random() * 4));
+                result_arr.push(random_num);
+                result_arrCounter++;
                
                    b4runblink=setInterval(function(){
                        $('#display').text('');
@@ -61,21 +79,48 @@ var gameInt;
                    var presetlv1=setTimeout(function(){
                     clearInterval(b4runblink);   
                     $('#display').text('1');
-                   }, 6500);
+                    $('#'+color_id[random_num]).css("background-color",color_arr[random_num]);
+                    $("#audio"+random_num).get(0).cloneNode().play();
+                    setTimeout(function(){
+                          $('#'+color_id[random_num]).css("background-color",orig_colors[random_num]);
+                    }, 1000);
+                        
+                   }, 5100);
                    
-                   gameInt=setInterval(function(){
-                    alert("in game interval");
-                        $('#display').text(round_level);
-                        random_num=Math.floor((Math.random() * 4));
-                        round_level+=1;
-                        $('#'+color_id[random_num]).css("background-color",color_arr[random_num]);
-                        $("#audio"+random_num).get(0).cloneNode().play();
-                        setTimeout(function(){
-                            $('#'+color_id[random_num]).css("background-color",orig_colors[random_num]);
-                        }, 1000);
-                   },15000);
+                   $('.fourcolors').css("pointer-events", "auto");
+                   $(".fourcolors").click(function(e){
+                       result_arrCounter=0;
+                       alert("user_clicked");
+                       var clickColor=e.target.id;
+                       switch(clickColor){
+                        case "blue":
+                            clicked_num=0;
+                           
+                            break;
+                        case "green":
+                            clicked_num=1;
+                            
+                            break;
+                        case "red":
+                            clicked_num=2;
+                            
+                            break;
 
-                  
+                        case "yellow":
+                            clicked_num=3;
+                          
+                            break;
+                       }
+
+                     $("#audio"+clicked_num).get(0).cloneNode().play();
+
+                     round_level++;
+                     user_clickedCounter++;
+
+
+
+                   });
+                
                    
              }
            
@@ -83,9 +128,6 @@ var gameInt;
 
 
 
-    $('.fourcolors').click(function(e){
-        alert(e.target.id);
-    });
 
 
     $('#switchoff').click(function(){
@@ -99,7 +141,7 @@ var gameInt;
        $('#inner_start').css("pointer-events", "auto");
        $('#inner_strict').css("pointer-events", "auto");
      
-       $('.fourcolors').css("pointer-events", "auto");
+       
 
 
       $('#display').css('color', 'red');
