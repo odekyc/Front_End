c41fbc76fa46ec462593e46f988b499f687de223
odekyc <odekyc@gmail.com>
2017-02-01 00:35:53 -0800

    hi

--- Tic_Tac_Toe/tic_tac_toe.js
+++ Tic_Tac_Toe/tic_tac_toe.js
@@ -1,3 +1,6 @@
+
+$(document).ready(function() {
+
 var x;
 var y;
 var xQuadrant;
@@ -14,10 +17,7 @@ var arr_len=placed_arr.length;
 var rev_xy;
 var occupied_arr_user=[];
 var occupied_arr_AI=[];
-
-$(document).ready(function() {
-
-    genIndex=Math.floor((Math.random() * arr_len) + 1)-1;
+genIndex=Math.floor((Math.random() * arr_len) + 1)-1;
     
  
  
@@ -30,64 +30,88 @@ $(document).ready(function() {
             context.beginPath();
          context.moveTo(70, 80);
          context.lineTo(420, 80);
+          context.lineWidth = 10;
+          context.strokeStyle="blue";
           context.stroke();
-          alert("AI WINS");
+          
+          $("#whowinsdiv").css("visibility","visible");
           occupied_arr_AI=[]
         }
         else if((occupied_arr_AI.indexOf('12')>-1)&&(occupied_arr_AI.indexOf('22')>-1)&&(occupied_arr_AI.indexOf('32')>-1)){
             context.beginPath();
          context.moveTo(70, 250);
          context.lineTo(420, 250);
+          context.lineWidth = 10;
+          context.strokeStyle="blue";
           context.stroke();
-          alert("AI WINS");
+          
+          $("#whowinsdiv").css("visibility","visible");
           occupied_arr_AI=[]
         }
         else if((occupied_arr_AI.indexOf('13')>-1)&&(occupied_arr_AI.indexOf('23')>-1)&&(occupied_arr_AI.indexOf('33')>-1)){
         context.beginPath();
          context.moveTo(70, 420);
          context.lineTo(420, 420);
+          context.lineWidth = 10;
+           context.strokeStyle="blue";
           context.stroke();
-          alert("AI WINS");
+         
+          $("#whowinsdiv").css("visibility","visible");
           occupied_arr_AI=[]
         }
         else if((occupied_arr_AI.indexOf('11')>-1)&&(occupied_arr_AI.indexOf('12')>-1)&&(occupied_arr_AI.indexOf('13')>-1)){
             context.beginPath();
          context.moveTo(76, 80);
          context.lineTo(76, 420);
+          context.lineWidth = 10;
+          context.strokeStyle="blue";
           context.stroke();
-          alert("AI WINS");
+          
+          $("#whowinsdiv").css("visibility","visible");
           occupied_arr_AI=[]
         }
         else if((occupied_arr_AI.indexOf('21')>-1)&&(occupied_arr_AI.indexOf('22')>-1)&&(occupied_arr_AI.indexOf('23')>-1)){
             context.beginPath();
          context.moveTo(246, 80);
          context.lineTo(246, 420);
+          context.lineWidth = 10;
+          context.strokeStyle="blue";
           context.stroke();
-          alert("AI WINS");
+          
+          $("#whowinsdiv").css("visibility","visible");
           occupied_arr_AI=[]
         }
         else if((occupied_arr_AI.indexOf('31')>-1)&&(occupied_arr_AI.indexOf('32')>-1)&&(occupied_arr_AI.indexOf('33')>-1)){
             context.beginPath();
          context.moveTo(418, 80);
          context.lineTo(418, 420);
+          context.lineWidth = 10;
+          context.strokeStyle="blue";
           context.stroke();
-          alert("AI WINS");
+          
+          $("#whowinsdiv").css("visibility","visible");
           occupied_arr_AI=[]
         }
         else if((occupied_arr_AI.indexOf('11')>-1)&&(occupied_arr_AI.indexOf('22')>-1)&&(occupied_arr_AI.indexOf('33')>-1)){
             context.beginPath();
          context.moveTo(70, 80);
          context.lineTo(418, 420);
+          context.lineWidth = 10;
+          context.strokeStyle="blue";
           context.stroke();
-          alert("AI WINS");
+          
+          $("#whowinsdiv").css("visibility","visible");
           occupied_arr_AI=[]
         }
         else if((occupied_arr_AI.indexOf('31')>-1)&&(occupied_arr_AI.indexOf('22')>-1)&&(occupied_arr_AI.indexOf('13')>-1)){
             context.beginPath();
          context.moveTo(418, 80);
          context.lineTo(70, 420);
+          context.lineWidth = 10;
+           context.strokeStyle="blue";
           context.stroke();
-          alert("AI WINS");
+         
+          $("#whowinsdiv").css("visibility","visible");
           occupied_arr_AI=[]
         }
 
@@ -95,64 +119,92 @@ $(document).ready(function() {
             context.beginPath();
          context.moveTo(70, 80);
          context.lineTo(420, 80);
+           context.lineWidth = 10;
+            context.strokeStyle="blue";
           context.stroke();
-          alert("USER WINS");
+           
+          $("#whowinsdiv").css("visibility","visible");
+          $("#winnername").text("User");
           occupied_arr_user=[];
         }
         else if((occupied_arr_user.indexOf('12')>-1)&&(occupied_arr_user.indexOf('22')>-1)&&(occupied_arr_user.indexOf('32')>-1)){
             context.beginPath();
          context.moveTo(70, 250);
          context.lineTo(420, 250);
+          context.lineWidth = 10;
+          context.strokeStyle="blue";
           context.stroke();
-          alert("USER WINS");
+          
+          $("#whowinsdiv").css("visibility","visible");
+          $("#winnername").text("User");
           occupied_arr_user=[];
         }
         else if((occupied_arr_user.indexOf('13')>-1)&&(occupied_arr_user.indexOf('23')>-1)&&(occupied_arr_user.indexOf('33')>-1)){
         context.beginPath();
          context.moveTo(70, 420);
          context.lineTo(420, 420);
+          context.lineWidth = 10;
+          context.strokeStyle="blue";
           context.stroke();
-          alert("USER WINS");
+          $("#whowinsdiv").css("visibility","visible");
+          $("#winnername").text("User");
           occupied_arr_user=[];
         }
         else if((occupied_arr_user.indexOf('11')>-1)&&(occupied_arr_user.indexOf('12')>-1)&&(occupied_arr_user.indexOf('13')>-1)){
             context.beginPath();
          context.moveTo(76, 80);
          context.lineTo(76, 420);
+          context.lineWidth = 10;
+          context.strokeStyle="blue";
           context.stroke();
-          alert("USER WINS");
+          
+          $("#whowinsdiv").css("visibility","visible");
+          $("#winnername").text("User");
           occupied_arr_user=[];
         }
         else if((occupied_arr_user.indexOf('21')>-1)&&(occupied_arr_user.indexOf('22')>-1)&&(occupied_arr_user.indexOf('23')>-1)){
             context.beginPath();
          context.moveTo(246, 80);
          context.lineTo(246, 420);
+          context.lineWidth = 10;
+          context.strokeStyle="blue";
           context.stroke();
-          alert("USER WINS");
+          
+          $("#whowinsdiv").css("visibility","visible");
+          $("#winnername").text("User");
           occupied_arr_user=[];
         }
         else if((occupied_arr_user.indexOf('31')>-1)&&(occupied_arr_user.indexOf('32')>-1)&&(occupied_arr_user.indexOf('33')>-1)){
             context.beginPath();
          context.moveTo(418, 80);
          context.lineTo(418, 420);
+          context.lineWidth = 10;
+          context.strokeStyle="blue";
           context.stroke();
-          alert("USER WINS");
+          $("#whowinsdiv").css("visibility","visible");
+          $("#winnername").text("User");
           occupied_arr_user=[];
         }
         else if((occupied_arr_user.indexOf('11')>-1)&&(occupied_arr_user.indexOf('22')>-1)&&(occupied_arr_user.indexOf('33')>-1)){
             context.beginPath();
          context.moveTo(70, 80);
          context.lineTo(418, 420);
+          context.lineWidth = 10;
+          context.strokeStyle="blue";
           context.stroke();
-          alert("USER WINS");
+          $("#whowinsdiv").css("visibility","visible");
+          $("#winnername").text("User");
           occupied_arr_user=[];
         }
         else if((occupied_arr_user.indexOf('31')>-1)&&(occupied_arr_user.indexOf('22')>-1)&&(occupied_arr_user.indexOf('13')>-1)){
             context.beginPath();
          context.moveTo(418, 80);
          context.lineTo(70, 420);
+          context.lineWidth = 10;
+          context.strokeStyle="blue";
           context.stroke();
-          alert("USER WINS");
+          $("#whowinsdiv").css("visibility","visible");
+          $("#winnername").text("User");
           occupied_arr_user=[];
         }
     }
@@ -289,5 +341,33 @@ $(document).ready(function() {
      
     });
 
-  
+ $("button").click(function(){
+      
+       context.clearRect(0, 0, canvas.width, canvas.height);
+       $("#whowinsdiv").css("visibility","hidden");
+       $(".in").text("");
+  x=0;
+y=0;
+xQuadrant=0;
+yQuadrant=0;
+x_or_o="";
+placed_arr=['11','12','13','21','22','23','31','32','33'];
+already_placed=false;
+clickIndex=0;
+numOccupied=0;
+element_str="";
+xystr="";
+arr_len=placed_arr.length;
+rev_xy="";
+occupied_arr_user=[];
+occupied_arr_AI=[];
+genIndex=Math.floor((Math.random() * arr_len) + 1)-1;
+   numOccupied+=1;
+  rev_xy=placed_arr[genIndex].split('').reverse().join('');
+
+    element_str+="#q"+rev_xy+"in";
+  $("#popup").css("visibility", "visible");
+        $("body").css("background-color", "#999999"); 
+});
+
 });
