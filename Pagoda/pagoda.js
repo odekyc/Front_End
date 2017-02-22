2d952aa10d13bee47a8075849c1a34b5ec822881
odekyc <odekyc@gmail.com>
2017-02-21 17:57:08 -0800

    hi

---
+++ Pagoda/pagoda.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	var stage = document.getElementById('pagoda');
+
+	document.body.addEventListener('mousemove', _.debounce(e => {
+	var xOrigin = stage.offsetWidth / 2;
+	var yOrigin = stage.offsetHeight / 2;
+	var xRot = (e.clientY - yOrigin) / yOrigin*-5+5;
+	var yRot = (e.clientX - xOrigin) / xOrigin * 20;
+	stage.style.transform = `rotateX(${xRot}deg) rotateY(${yRot}deg)`;
+	}), 500);
+
+});
